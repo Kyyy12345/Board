40e40dc74db790fbb1129d6546a6c5ba87261b2d
Kyyy12345 <127706140+Kyyy12345@users.noreply.github.com>
2025-03-24 21:22:46 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+	@GetMapping("/") // 현재경로로 접근
+	public String index() {
+		return "index"; // jsp 파일명 적기
+	}
+	
+	
+}
